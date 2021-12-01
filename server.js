6eae760276fa21bd688c5f33e7ae091b58f3163d
mehdijebali <69167071+mehdijebali@users.noreply.github.com>
2021-12-01 11:45:13 +0000

    Set up passport

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
+
+app.use(session({
+  secret: ,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,3 +36,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+
+
