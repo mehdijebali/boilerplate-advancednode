35b9fcbd96d2f03192878b05ad9b4ea646866669
mehdijebali <69167071+mehdijebali@users.noreply.github.com>
2021-12-01 12:44:14 +0000

    Set up DataBase

--- server.js
+++ server.js
@@ -19,14 +19,26 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
 passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 
 passport.deserializeUser((id, done) => {
-  //myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
     done(null, null);
-  //});
+  });
 });
 
 app.set('view engine', 'pug');
@@ -42,6 +54,12 @@ app.route('/').get((req, res) => {
     title: 'Hello',
     message: 'Please login'});
   
+});
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
